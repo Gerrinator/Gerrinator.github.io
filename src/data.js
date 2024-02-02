8431ae90b29fa5a53fc13dacaf6c67c7ebf11838
Gerrinator <vangenderengerard@gmail.com>
2024-02-02 10:30:50 -0700

    Added more game projects and skills to data.js

--- src/data.js
+++ src/data.js
@@ -6,12 +6,37 @@ export const game_projects = [
       subtitle: "Music Composer, Sound Designer, Lead Designer",
       description:
         "A Sci-fi horror puzzle game made in RPG maker.",
-      image: "./curiosity.png",
+      image: "./curiosity.gif",
       link: "https://tienducho.itch.io/curiosity-kills-the-cat",
+    },
+    {
+      title: "The Artifact",
+      subtitle: "Lead Programmer, Designer", 
+      description: "An interactive audio experience made using Unreal Engine",
+      image: "artifact.gif",
+      link: "https://github.com/Gerrinator/TheArtifact",
+    },
+    {
+      title: "Prodigy Bot",
+      subtitle: "Programmer",
+      description: "A competitive bot for Starcraft II",
+      image: "starcraft.png",
+      link: "https://github.com/apollo-online/ProdigyBot",
+    },
+    {
+      title: "Specimen X: Escape from the Lab",
+      subtitle: "Composer, Programmer, Sound Designer",
+      description: "Sci-fi horror game submitted for the Alberta Game Jam 2023",
+      image: "./specimenx.gif",
+      link: "https://ralworks.itch.io/specimenx",
     }
   ];
 
 export const skills = [
     "Java",
-    "C++"
+    "C++",
+    "C#",
+    "Unity",
+    "Unreal Engine",
+    "Godot",
 ]
